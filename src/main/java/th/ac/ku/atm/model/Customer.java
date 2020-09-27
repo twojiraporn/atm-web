25f91b13f593908ad75f29ab600d9356f0647738
twojiraporn <jiraporn.kow>
2020-09-27 09:48:50 +0700

    change db connection from jbdc to jpa

--- src/main/java/th/ac/ku/atm/model/Customer.java
+++ src/main/java/th/ac/ku/atm/model/Customer.java
@@ -1,9 +1,18 @@
 package th.ac.ku.atm.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+
+    @Id
     private int id;
+
     private String name;
     private String pin;
+
+    public Customer() {}
 
     public Customer(int id, String name, String pin) {
         this.id = id;
@@ -15,8 +24,16 @@ public class Customer {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPin() {
